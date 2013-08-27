1bb0fb1d4fa9eb227a662bd513cc14dc7a0b81cf
Richard Balson <richard.balson@gmail.com>
2013-08-27 15:54:17 +1000

    State estimation with arying levels of noise testing

--- User_defined_parameters.m
+++ User_defined_parameters.m
@@ -10,18 +10,18 @@
 
 filter_simulation =0; % Specify whether or not to filter simulated data
 
-Data_scale =0.2; % Scale simulation output to determine the effect of scaling on estimation out = outOrig/Data_scale
+Data_scale =1; % Scale simulation output to determine the effect of scaling on estimation out = outOrig/Data_scale
 
 if filter_simulation
-    highcutoff = 2.5; % Specify highcutoff frequency for filter
+    highcutoff = 1.5;%0.5;%2.5; % Specify highcutoff frequency for filter
     
-    lowcutoff = 40; % Specify low cutoff frequency for filter
+    lowcutoff = 250;%80;%40; % Specify low cutoff frequency for filter
     % Data will have frequency content between highcutoff and lowcutoff
 end
 
 fs =2048; % Specify sampling frequency
 
-Simulation_number = 20; % Specify number of simulations to estimate
+Simulation_number = 1; % Specify number of simulations to estimate
 
 Random_number_generator=[0 0]; % Specify whether random numbers for simulation should be different or the same for numerous simulations;
 
@@ -42,14 +42,14 @@ LimitEst = 0; % If set to 1 estimation results are limited by physiological para
 
 simulate =1; % Decide whether to simulate model output or use previous results if simulate is equal to 1 then observations for estimatio purposes are resimulated
 
-NoiseIn = 1e-3;% Base 1e-2 Specify noise to add to simulated signal
+NoiseIn = 1e1;%1e-3;% Base 1e-2 Specify noise to add to simulated signal
 SimulationSettings.name = 'Wendling_output'; % Specify name of file to save Wendling model output data to, or to load data from when simulation is not performed
 if simulate % Specify parameters for simulation purposes
     SimulationSettings.simulation_time =100; %Time for simulation in seconds 
     SimulationSettings.slope_time =1; % Specifies the time over which the model gain should be altered
     SimulationSettings.number_of_sigma_input = 1; % Used to determine standard deviation of input if  1: 68.27% of realisations within physiolgical range, 2: 95.45, 3: 99.73 4: 99.994
     SimulationSettings.stochastic = 1; % Used to specifiy the stochastic adjustment on the input 1 is no adjustment. <1 downscalling, >1 upscaling
-    SimulationSettings.Parameter_index = 7; % Choose parameters to be simulated: 1 = Seizure Parameter from Wendling 2002;
+    SimulationSettings.Parameter_index = 4; % Choose parameters to be simulated: 1 = Seizure Parameter from Wendling 2002;
     %  2 = Seizure Parameter from Wendling 2005;...
     %  3 = Altered excitability;
     %  4 = Parameters at midpoint of their range;
@@ -58,9 +58,9 @@ if simulate % Specify parameters for simulation purposes
     %  variations in simulation
     % 7 User defined
     if SimulationSettings.Parameter_index % Specify synaptic gains for simulation
-        SimulationSettings.AV= [5 5 5 5 5 7 5];%[4 4 4 4 5 5 6 6];
-        SimulationSettings.BV= [20 20 20 30 20 20 20];%[15 15 15 25 25 30 30 30];
-        SimulationSettings.GV= [20 30 20 20 20 20 20];%[30 30 30 25 25 20 20 20];
+        SimulationSettings.AV= [4 4 4 4 5 5 6 6];%[5 5 5 5 5 7 5];%[4 4 4 4 5 5 6 6];
+        SimulationSettings.BV= [15 15 15 25 25 30 30 30];%[20 20 20 30 20 20 20];%[15 15 15 25 25 30 30 30];
+        SimulationSettings.GV= [30 30 30 25 25 20 20 20];%[20 30 20 20 20 20 20];%[30 30 30 25 25 20 20 20];
     end
     SimulationSettings.Input_mean_variation = 0; % If 0 mean stays constant for simulation,
     %if 1 input mean is drawn from a uniform distribution limited by the physiological limits of the input
@@ -85,9 +85,9 @@ EstStart = 5; % Specify the duration after simulation start when estimation shou
 
 Ds = 8; % Number of differential equations describing model, also the number of fast states to be estiamted
 
-Dp = 3; % Number of parameters to be estimated, also refered to as slow states
+Dp = 0; % Number of parameters to be estimated, also refered to as slow states
 
-Dk =1; %If set to 1 the mean of the stochastic input will be estimated % Note that if Input_mean_variation is not zero than Dk should be set to one to allow tracking of the input mean
+Dk =0; %If set to 1 the mean of the stochastic input will be estimated % Note that if Input_mean_variation is not zero than Dk should be set to one to allow tracking of the input mean
 
 Dy =1; % Number of observable outputs from the simulation
 
@@ -126,7 +126,7 @@ Base_input_uncertainty = 1e-12;%1e-3; % Inherent parameter uncertainty due to mo
 
 Variable_input_uncertainty =0;%1e-3; % Uncertianty due varying input mean, Set to zero if the input mean is not varying
 
-Observation_uncertainty = 1e-3; % Specify the uncertainty in observations
+Observation_uncertainty = 1e1; % Specify the uncertainty in observations
 
 uncertainty_adjustment = 1; % Adjuster for model uncertainty
 
@@ -146,7 +146,7 @@ fig_save =1; % Save figures as .fig for future use
 
 printpdf =1; % Specify whether results should be printed to pdf
 
-Image_handling_model_output=[1;0];
+Image_handling_model_output=[1;1];
 
 plot_uncertainty =1; % Plot covariance of all states
 
@@ -164,7 +164,7 @@ Image_handling_firing_rates = [0 0 0 0]; % Here a decision is made whether to pl
                                                                             % The columns indicate the firing rate to plot. this is a three image plot where the input potential population firing rate and output potential are plotted.
                                                                             % Here column 1-4 are Vp,Ve,Vsi and Vfi respectively.
 
-Image_handling_multi = [1 0 1;1 0 0];%  % Here a decision is made whether to plot specific states, 
+Image_handling_multi = [1 1 1;1 0 0];%  % Here a decision is made whether to plot specific states, 
                                                                             % if the value is one the relevant figure is plotted, otherwise it is not.
                                                                             % The columns indicate the figures to be plotted.
                                                                             % Here column 1-3 are for all the model states, all the model states inputs, all the model parameters including the input mean.
